cd0de0dfe3beefd481fdcc2c7d700423c4a6a6a3
xavilind <xavilind7@gmail.com>
2022-02-22 15:38:24 -0600

    Fragment

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,9 +1,17 @@
-import React from 'react';
+
+
+//import React, { Fragment } from 'react';
 
 
 const PrimeraApp=()=> {
 
-return <h1>Hola Mundo</h1>;
+return (
+     <>
+    <h1>Hola Mundo</h1>
+     <p>Mi primera aplicacion</p>
+     </>
+);
+
 }
 
 export default PrimeraApp;
